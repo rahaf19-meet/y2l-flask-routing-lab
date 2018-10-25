adf38e4b26355c0bc8847a1e5e30f90bef40f646
avigail18-meet <tutim2000@gmail.com>
2018-10-25 18:48:13 +0300

    fghj

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+    technology=["Phones","Laptops","Headphones"]
+    return render_template("home.html",technology= technology)
+
+@app.route('/shop')
+def shop():
+    return render_template("shop.html")
+
+@app.route('/lap')
+def lap():
+    return render_template("laptops.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
+
